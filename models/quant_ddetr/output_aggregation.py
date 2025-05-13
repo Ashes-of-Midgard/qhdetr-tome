b00f340f9c6f76893f80e6d5680d075a86e1f458
yyl <20231149@buaa.edu.cn>
2025-05-13 16:08:10 +0800

    Change class distance to sce

--- models/quant_ddetr/output_aggregation.py
+++ models/quant_ddetr/output_aggregation.py
@@ -3,32 +3,17 @@ from torch import nn
 import torch.nn.functional as F
 
 # from .lsq_plus import *
-from ._quan_base_plus import truncation
+# from ._quan_base_plus import truncation
 from util import box_ops
 
 
-def masked_kl_divergence(pred_logits: torch.Tensor, mask: torch.tensor) -> torch.Tensor:
-    result_matrix = torch.full_like(mask.to(torch.float), float('inf'))
-    mask_indices = torch.nonzero(mask, as_tuple=True)
-
-    if len(mask_indices[0]) > 0:
-        batch_idx, proposal_indices_row, proposal_indices_column = mask_indices
-
-        relevant_pred_logits_row = pred_logits[:, proposal_indices_row]
-        relevant_pred_logits_column = pred_logits[:, proposal_indices_column]
-
-        log_softmax_preds_row = F.log_softmax(relevant_pred_logits_row, dim=1)
-        softmax_preds_column = F.softmax(relevant_pred_logits_column, dim=1)
-
-        kl_divs = F.kl_div(
-            log_softmax_preds_row.unsqueeze(1),
-            softmax_preds_column.unsqueeze(0),
-            reduction='none'
-        )
-        # print(batch_idx, proposal_indices_row, proposal_indices_column, kl_divs)
-        result_matrix[batch_idx, proposal_indices_row, proposal_indices_column] = kl_divs.mean(dim=-1)
-
-    return result_matrix
+def symmetric_cross_entropy(p, q, epsilon=1e-8):
+    q = q + epsilon
+    p = p + epsilon
+    ce_pq = (-p * q.log()).sum(dim=-1)
+    ce_qp = (-q * p.log()).sum(dim=-1)
+    sce = ce_pq + ce_qp
+    return sce
 
 
 class OutAggregate(nn.Module):
@@ -51,11 +36,10 @@ class OutAggregate(nn.Module):
             iou_matrix = torch.stack(iou_matrix)
             iou_matrix_gt_threshold = iou_matrix > self.t_b
 
-            kl_div_matrix = torch.nn.functional.kl_div(prob.unsqueeze(2), prob.unsqueeze(1), reduction='none')
-            kl_div_matrix = kl_div_matrix.mean(-1)
-            print(f"kl_div_matrix:\n{kl_div_matrix}")
-            print(kl_div_matrix.shape)
-            kl_div_matrix_lt_threshold = kl_div_matrix < self.t_c
+            sce_matrix = symmetric_cross_entropy(prob.unsqueeze(2), prob.unsqueeze(1))
+            print(f"sce_matrix:\n{sce_matrix}")
+            print(sce_matrix.shape)
+            kl_div_matrix_lt_threshold = sce_matrix < self.t_c
             
             aggregation_mask = iou_matrix_gt_threshold & kl_div_matrix_lt_threshold
             aggregation_mask = aggregation_mask | aggregation_mask.T # to ensure mask is symmetric
